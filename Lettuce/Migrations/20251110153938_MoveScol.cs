c47d4281edb797a5fbcde9d4b45126ced9c670f8
Emilia <emilia@jumpsca.re>
2025-11-10 17:40:52 +0200

    Move supreme court out of gift range

---
+++ Lettuce/Migrations/20251110153938_MoveScol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lettuce.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveScol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Pawns",
+                keyColumn: "Id",
+                keyValue: new Guid("ffffffff-ffff-ffff-ffff-ffffffffffff"),
+                columns: new[] { "X", "Y" },
+                values: new object[] { -8, 11 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Pawns",
+                keyColumn: "Id",
+                keyValue: new Guid("ffffffff-ffff-ffff-ffff-ffffffffffff"),
+                columns: new[] { "X", "Y" },
+                values: new object[] { -5, 5 });
+        }
+    }
+}
